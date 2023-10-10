e4b22327e2e7b3bdde7ed756a4b38bba39d0ee09
Thomas LaPinta <tlapinta2002@gmail.com>
2023-10-09 21:49:16 -0400

    Added the ability to email the log file to other machines

--- MatchupScraper/main.py
+++ MatchupScraper/main.py
@@ -1,6 +1,9 @@
 from matchupScraper import *
 from datetime import *
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import os
+import smtplib
 
 def main():
     #Creates log file to store the stack trace and errors
@@ -19,6 +22,45 @@ def main():
         print('Process completed successfully!')
         log_file.write('Process completed successfully!\n')
     log_file.close()
+    
+    #Instanciate the email credentials for sftp connection
+    curr_dir = os.path.dirname(__file__)
+    path = os.path.join(curr_dir, 'Env/email.env')
+    load_dotenv(path)
+
+    email_sender = os.getenv('email_sender')
+    email_password = os.getenv('email_password')
+    email_port = os.getenv('email_port')
+    email_receiver = os.getenv('email_receiver')
+
+    print(email_sender)
+    print(email_password)
+    print(email_port)
+    print(email_receiver)
+
+    #Connects to the SFTP server
+    server = smtplib.SMTP('smtp.gmail.com', email_port)
+    server.starttls()
+    server.login(email_sender, email_password)
+
+    #Instanciates the messahe as an object
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Matchup Scraper Output ' + str(date.today())
+    msg['From'] = email_sender
+    msg['To'] = email_receiver
+
+    #Reads the log file and attaches to the email
+    with open(log_path, 'rb') as file:
+        attachment = MIMEText(file.read(), 'plain', 'uft-8')
+        attachment.add_header('Content-Disposition', 'attachment', filename=os.path.basename(log_path))
+        msg.attach(attachment)
+
+    #Sends the email
+    server.sendmail(email_sender, email_receiver, msg.as_string())
+
+    #Disconnects from the server
+    server.quit()
+    
     return 0
         
 #Main Method
